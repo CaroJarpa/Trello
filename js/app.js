41e9fcff2ba5f8cd8d77e8ea4b016e514f5d2777
carolina <carolina.jarpam@gmail.com>
2017-11-08 02:01:00 -0300

    archivos incompletos

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* primero se llama al padre que recibira el texto ingresado*/
+
+var add = document.getElementById('mylist');
+
+/*usar el div que tiene el boton guardar*/
+var boton = document.getElementById("guardar");
+
+/*ahora se dan las instrucciones para cuando
+se haga click en el boton*/
+
